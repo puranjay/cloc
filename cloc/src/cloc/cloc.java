d85ae701390887a24c5feea67e6555b19b2c9cae
puranjay <puranjay.andr@gmail.com>
2014-03-29 21:11:51 +0530

    More cleanup and formatting

--- cloc/src/cloc/cloc.java
+++ cloc/src/cloc/cloc.java
@@ -13,11 +13,11 @@ public class cloc {
 
 	public static void main(String[] args) throws IOException {
 
-		String filename; // Stores the name of the input file
-		int emptylines = 0; // Stores the number of empty lines
-		int codelines = 0; // Stores the number of code lines
-		int commentlines = 0; // Stores the number of comment lines
-		int comflag = 0; // Flag is -1 if a comment is currently open, 0 if not
+		String filename;  	//Stores the name of the input file
+		int emptylines = 0;	//Stores the number of empty lines
+		int codelines = 0;	//Stores the number of code lines
+		int commentlines = 0;	//Stores the number of comment lines
+		int comflag = 0;	//Flag is -1 if a comment is currently open, 0 if not
 
 		/* Get file name input */
 		try (Scanner inp = new Scanner(System.in)) {
@@ -28,47 +28,39 @@ public class cloc {
 			return;
 		}
 
-		/* Use Buffered Reader to read the input file line by line */
+		/* Use Buffered Reader to read the input file line by line*/
 		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-			/*
-			 * Read the file line-by-line and store current line as a String
-			 * variable 'line'
-			 */
+			/*Read the file line-by-line and store current line as a String variable 'line'*/
 			for (String line; (line = br.readLine()) != null;) {
 
-				/* Check if the current line is empty */
+				/*Check if the current line is empty*/
 				if (line.isEmpty())
 					emptylines++;
 
 				else {
-					/* Check if the line starts a comment */
+					/*Check if the line starts a comment*/
 					if (line.trim().startsWith("/*")) {
-						comflag = -1; // Indicates that a comment has begun and
-										// is still open
+						comflag = -1;  //Indicates that a comment has begun and is still open
 						commentlines++;
 					}
 
-					/* Check if a comment is still open */
+					/*Check if a comment is still open*/						
 					else if (comflag == -1) {
-						/* Check if the comment is continued */
+						/*Check if the comment is continued*/
 						if (line.trim().startsWith("*")) {
 							commentlines++;
-						} else
-							comflag = 0; // If comment is closed, set flag back
-											// to zero
+						}
+						else
+							comflag = 0; //If comment is closed, set flag back to zero
 					}
 
-					/*
-					 * If the line is neither empty, not a comment, add it to
-					 * LOC
-					 */
+					/*If the line is neither empty, not a comment, add it to LOC*/
 					else if (comflag == 0)
 						codelines++;
 				}
 			}
 		} catch (FileNotFoundException e) {
-			System.out
-					.println("\nThe file could not be found. Please check the name and try again.");
+			System.out.println("\nThe file could not be found. Please check the name and try again.");
 			return;
 		}
 
